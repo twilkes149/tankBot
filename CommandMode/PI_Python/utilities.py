5b4566a6dad1d65770bc26a17ed8499aa4dd13dc
Tucker Wilkes <twilkes149@gmail.com>
2020-04-19 15:20:51 -0700

    added utilities node

---
+++ CommandMode/PI_Python/utilities.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import rospy
+import os
+
+from std_msgs.msg import String
+
+def shutdown_callback(message):
+	os.system("shutdown")
+
+if __name__ == "__main__":
+	rospy.init_node('utilities')
+	subscriber = rospy.Subscriber('/shutdown_cmd', String, shutdown_callback)
+	rospy.spin()
